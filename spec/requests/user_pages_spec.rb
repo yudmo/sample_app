3c296e362f197a4e71534763f3fa897b2a21d9c2
Yudai MORIYA <s1520699@u.tsukuba.ac.jp>
2015-07-19 14:13:34 +0900

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,12 +5,12 @@ describe "User pages" do
   subject { page }
 
   describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  it { should have_content(user.name) }
-  it { should have_title(user.name) }
-end
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
@@ -41,6 +41,15 @@ end
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
